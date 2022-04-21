757cfb22afbbb1771a3f416dfc8fb68a9ee88d82
JuanG <Juanc43092@gmail.com>
2022-04-21 12:53:42 -0700

    Added some logic, still not returning the result. Instead refreshes button.

--- rpsg.js
+++ rpsg.js
@@ -5,14 +5,51 @@
 -make playerSelection parameter case-insensitive 
 -return not console */
 
-
-const myArray = ['Rock', 'Paper', 'Scissors'];
-function computerPlay() {
-
-return myArray[~~(Math.random() * myArray.lenth)];
-}
-
 function playRound(playerSelection, computerSelection){
+  
+   let playRound = prompt("Rock, paper, or scissors?");
+    if (playerSelection) {
+        let playerOne = playerSelection.trim() .toLowerCase();
+         if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors"){
 
-    
+             let computerSelection = Math.floor(Math.random()* 3 + 1);
+             let computer = computerSelection === 1 ? "rock"
+                : computerSelection === 2 ? "paper"
+                :"scissors";
+
+             let result = 
+             playerSelection === computer 
+                ? "Tie Game!"
+                : playerSelection === "rock" && computer === "paper"
+                ? `playerSelection: ${playerSelection}\nComputer: ${computer}\nComputer wins!`
+                : playerSelection === "paper" && computer === "scissors"
+                ? `playerSelection: ${playerSelection}\nComputer: ${computer}\nComputer wins!`
+                : playerSelection === "scissors" && computer === "rock"
+                ? `playerSelection: ${playerSelection}\nComputer: ${computer}\nComputer wins!`
+                : `playerSelection: ${playerSelection}\nComputer: ${computer}\nplayerSelection Wins!`;
+             alert(result);
+             let playAgain = cofirm("Play again?");
+             playAgain ? location.reload(): alert("Ok, thanks for playing.");
+            }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
