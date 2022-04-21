34ce018063606af01ec383a23daa713f13e2fe36
JuanG <Juanc43092@gmail.com>
2022-04-20 17:41:51 -0700

    added psuedocode and started. '

---
+++ rpsg.js
@@ -0,0 +1,11 @@
+/* -randomly return "rock,paper,scissors"
+-function is 1 round
+-2 parameters: playerSelection & computerSelection
+-return a string declaring winner of the round; ex. "you lose, paper beats rock
+-make playerSelection parameter case-insensitive 
+-return not console */
+
+function computerplay() {
+
+    
+}
